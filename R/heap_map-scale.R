755830b7764937aff0ab8c8bc137d53090dd57c0
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2020-07-17 10:33:11 +1000

    Speed boost to heat_map_scale.

--- R/heap_map-scale.R
+++ R/heap_map-scale.R
@@ -1,9 +1,9 @@
 ## HEAT_MAP_SCALE --------------------------------------------------------------
 
-#' Scale numeric data prior to constructing heatmap
+#' Scale numeric data prior to constructing heat_map
 #'
 #' Apply column-wise or row-wise scaling to numeric columns in a matrix or
-#' data.frame prior to constructing a heatmap.
+#' data.frame prior to constructing a \code{heat_map}.
 #'
 #' @param x matrix-like object to be scaled.
 #' @param scale indicates whether the data should be scaled by \code{"row"} or
@@ -33,89 +33,46 @@
 heat_map_scale <- function(x,
                            scale = "column",
                            method = "range") {
-
-  # ROWNAMES
-  if (is.null(rownames(x))) {
-    rownames(x) <- seq_len(nrow(x))
-  }
-
-  # COLNAMES
-  if (is.null(colnames(x))) {
-    colnames(x) <- seq_len(ncol(x))
-  }
-
-  # SCALE NUMERIC COLUMNS ONLY
-  num_cols <- which(unlist(lapply(seq_len(ncol(x)), function(z) {
-    is.numeric(x[, z])
-  })))
   
-  # COLUMN-WISE SCALING
+  # SCALE NUMERIC COLUMNS ONLY
+  num_cols <- which(apply(x, 2, is.numeric))
+  num_data <- x[, num_cols]
+  
+  # SCALING
   if(grepl("^c", scale, ignore.case = TRUE)){
-    # MESSAGE
-    message(paste0(
-      "Applying ",
-      method,
-      " scaling to each column..."
-    ))
-    # COLUMN SCALING
-    lapply(num_cols, function(z) {
-      # RANGE
-      if (grepl("range", method, ignore.case = TRUE) |
-          grepl("^r", method, ignore.case = TRUE)) {
-        x[, z] <<- .scale_range(x[, z])
-      # MEAN
-      } else if (grepl("mean", method, ignore.case = TRUE) |
-                 grepl("^m", method, ignore.case = TRUE)) {
-        x[, z] <<- .scale_mean(x[, z])
-      # Z-SCORE
-      } else if (grepl("^z", method, ignore.case = TRUE)) {
-        x[, z] <<- .scale_zscore(x[, z])
-      } else {
-        stop(paste0(method, " is not a supported scaling method."))
-      }
-    })
-  # ROW-WISE SCALING  
-  }else if(grepl("^r", scale, ignore.case = TRUE)){
-    # MESSAGE
-    message(paste0(
-      "Applying ",
-      method,
-      " scaling to each row..."
-    ))
-    # RESTRICT TO NUMERIC DATA
-    x_num <- x[, num_cols]
-    # TRANSPOSE
-    x_num_trans <- t(x_num)
-    # NUMERIC COLUMNS (IN CASE)
-    lapply(ncol(x_num_trans), function(z){
-      x_num_trans[, z] <<- as.numeric(x_num_trans[, z])
-    })
-    # COLUMN SCALING
-    lapply(seq_len(ncol(x_num_trans)), function(z) {
-      # RANGE
-      if (grepl("range", method, ignore.case = TRUE) |
-          grepl("^r", method, ignore.case = TRUE)) {
-        x_num_trans[, z] <<- .scale_range(x_num_trans[, z])
-        # MEAN
-      } else if (grepl("mean", method, ignore.case = TRUE) |
-                 grepl("^m", method, ignore.case = TRUE)) {
-        x_num_trans[, z] <<- .scale_mean(x_num_trans[, z])
-        # Z-SCORE
-      } else if (grepl("^z", method, ignore.case = TRUE)) {
-        x_num_trans[, z] <<- .scale_zscore(x_num_trans[, z])
-      } else {
-        stop(paste0(method, " is not a supported scaling method."))
-      }
-    })
-    # REVERSE TRANSPOSE
-    x_num <- t(x_num_trans)
-    # NUMERIC COLUMNS (IN CASE)
-    lapply(ncol(x_num), function(z){
-      x_num[, z] <<- as.numeric(x_num[, z])
-    })
-    # REPLACE NUMERIC COLUMNS
-    x[, num_cols] <- x_num
+    message(
+      paste0(
+        "Applying", method, " scaling to each column..."
+      )
+    )
+    scale <- 2
+  }else{
+    message(
+      paste0(
+        "Applying", method, " scaling to each column..."
+      )
+    )
+    scale <- 1
   }
+  
+  # SCALING
+  num_data <- apply(num_data, scale, function(x){
+    # RANGE SCALING
+    if(grepl("^r", method, ignore.case = TRUE)){
+      .scale_range(x)
+      # MEAN
+    }else if(grepl("^m", method, ignore.case = TRUE)){
+      .scale_mean(x)
+      # Z-SCORE
+    }else if(grepl("^z", method, ignore.case = TRUE)){
+      .scale_zscore(x)
+    }else{
+      paste0(method, " is not a supported scaling method.")
+    }
+  })
+  
+  # REPLACE ORIGINAL DATA
+  x[, num_cols] <- num_data
   
   # RETURN SCALED DATA
   return(x)
